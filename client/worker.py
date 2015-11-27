9047db4d2a36f39287f41dfd6ee2e4e9d2332570
Alan Pope <alan@popey.com>
2015-11-27 12:02:05 +0000

    Add various client end tools to bootstrap/test

--- client/worker.py
+++ client/worker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """The ubuntu-app-test worker.
 Expects to be invoked like this:
-python worker.py --server=http://frontend.example.com:12345/ --device="Bq E4.5 Ubuntu Edition" param1 param2 param3
+python worker.py --server=http://frontend.example.com:12345/ --device="Bq E4.5 Ubuntu Edition" <device_serial_number> <device_codename> <orientation>
 
 The server parameter dictates where to get jobs from.
 The device parameter is a name for this device; it is shown to users in the front end to choose
@@ -10,7 +10,7 @@ The other parameters are passed to the do_test() function, which uses them to kn
     which USB port its device is on, etc.
 """
 
-import argparse, traceback, sys, urllib, urlparse, time, json
+import argparse, traceback, sys, urllib, urlparse, time, json, subprocess
 
 fp = open("claim_secret") # this needs to exist. Put a long random string in it.
 claim_secret = fp.read()
@@ -20,15 +20,28 @@ fp.close()
 # Parts that need implementing
 ############################################################################################
 
+def do_provision(device):
+    print "**** Provisioning device ******"
+    print device
+    #provision_cmd = "./provision -s " + device + " -w -n $HOME/.ubuntu-ci/wifi.conf"
+    provision_cmd = "./provision -s " + device
+    print provision_cmd
+    subprocess.call(provision_cmd, shell=True)
+
 def do_test(params, job):
     print "****************** Actually running the test."
     print "* Data passed to run this job *"
     print params
     print job
     print "****************** So we will want to download %s" % (job["click"], )
-    print "****************** This needs to be implemented, and must be synchronous."
-    print "****************** We fake it by waiting four seconds."
-    time.sleep(4)
+    print "****************** This is implemented in runtest bash script."
+    print "****************** Which should move to this python script ASAP"
+    # We pass the url to the click, device serial number and type and orientation
+    # e.g. ./runtest /click/20151126103906-PLBRWIBL9X 0050aba613958223 mako portait
+    runtest_cmd = "./runtest " + job["click"] + " " + params[0] + " " + params[1] + " " + params[2]
+    print runtest_cmd
+    #subprocess.call(runtest_cmd, shell=True)
+    time.sleep(30)
     return True, {"screenshot": "whatever", "logfile": "whatever"}
 
 def deal_with_results(job, results):
@@ -73,6 +86,9 @@ def check_forever(server, device, test_params):
             job = get_job(server, device)
             if not job:
                 print "No job available: waiting %s seconds and trying again" % wait_time
+                # Check for device still existing and being available here.
+                # if it goes away, we die. Bootstrap should respawn us once the device
+                # comes back
                 time.sleep(wait_time)
                 wait_time = wait_time * 2
                 if wait_time > 250: wait_time = 250
@@ -84,11 +100,13 @@ def check_forever(server, device, test_params):
                 print "Job successfully executed. Releasing job."
                 release_job(server, job)
                 deal_with_results(job, results)
+                do_provision(device=args.params[0])
                 wait_time = 1
             else:
                 # wait for wait_time, because we did not succeed, meaning this job went wrong
                 print "Job failed: releasing job, then waiting %s seconds and trying again" % wait_time
                 release_job(server, job)
+                do_provision(device=args.params[0])
                 time.sleep(wait_time)
                 wait_time = wait_time * 2
                 if wait_time > 250: wait_time = 250
@@ -110,9 +128,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='The ubuntu-app-test worker.')
     parser.add_argument('params', metavar='N', nargs="*",
                    help='parameters to pass to the do_test function')
-    parser.add_argument('--server', dest='server', 
+    parser.add_argument('--server', dest='server',
         help='HTTP URL of the server', required=True)
-    parser.add_argument('--device', dest='device', 
+    parser.add_argument('--device', dest='device',
         help='User-viewable name for this device (need not be unique)', required=True)
     args = parser.parse_args()
+    print args
+    do_provision(device=args.params[0])
     check_forever(args.server, args.device, args.params)
